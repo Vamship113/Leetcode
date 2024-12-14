ce78423875536733398d57ba46b8edc3ac95e0d6
Vamship113 <92430906+Vamship113@users.noreply.github.com>
2024-12-14 20:46:35 +0530

    Time: 0 ms (100%), Space: 41.1 MB (87.71%) - LeetHub

--- 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.java
+++ 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.java
@@ -1,21 +1,32 @@
+// class Solution {
+//     public int isPrefixOfWord(String sentence, String searchWord) {
+//         int k=searchWord.length(),l=sentence.length();
+//         String test="";
+//         int loc=1;
+//         int i=0;
+//         while(i<l){
+//             char c=sentence.charAt(i);
+//             if(c!=' ') {
+//                 test+=c;
+//                 if(test.length()==k && test.equals(searchWord)) return loc;
+//             }
+//             else{
+//                 loc++;
+//                 test="";
+//             }
+//             i++;
+//         }
+//     return -1;
+//     }
+// }
 class Solution {
-    public int isPrefixOfWord(String sentence, String searchWord) {
-        int k=searchWord.length(),l=sentence.length();
-        String test="";
-        int loc=1;
-        int i=0;
-        while(i<l){
-            char c=sentence.charAt(i);
-            if(c!=' ') {
-                test+=c;
-                if(test.length()==k && test.equals(searchWord)) return loc;
+    public int isPrefixOfWord(String sentence, String searchWord){
+        String[] arr = sentence.split(" ");
+        for(int i=0;i<arr.length;i++){
+            if(arr[i].startsWith(searchWord)){
+                return i+1;
             }
-            else{
-                loc++;
-                test="";
-            }
-            i++;
         }
-    return -1;
+        return -1;
     }
 }
