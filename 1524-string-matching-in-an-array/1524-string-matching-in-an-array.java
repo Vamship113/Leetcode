c4dd5ac212f447414221d9ff962db102ba6b2229
Vamship113 <92430906+Vamship113@users.noreply.github.com>
2025-01-07 21:00:14 +0530

    Time: 3 ms (92.21%), Space: 42.6 MB (16.91%) - LeetHub

--- 1524-string-matching-in-an-array/1524-string-matching-in-an-array.java
+++ 1524-string-matching-in-an-array/1524-string-matching-in-an-array.java
@@ -1,15 +1,56 @@
 class Solution {
     public List<String> stringMatching(String[] words) {
         List<String> res = new ArrayList<>();
-        for(int i=0;i<words.length;i++){
-            for(int j=0;j<words.length;j++){
-                if(i!=j){
-                    if(words[i].contains(words[j]) && res.contains(words[j])==false){
-                        res.add(words[j]);
-                    }
+        for (int i = 0; i < words.length; i++) {
+            String pattern = words[i];
+            for (int j = 0; j < words.length; j++) {
+                if (i != j && words[j].contains(pattern)) {
+                    res.add(pattern);
+                    break;
                 }
             }
         }
         return res;
     }
+
+    	int[] computeLPS(String pattern) {
+    	int[] lps= new int[pattern.length()];
+    	int i=0,j=1;
+    	lps[0] =0;
+    	while(j<pattern.length()) {
+    		if(pattern.charAt(i)==pattern.charAt(j)) {
+    			i++;
+    			lps[j]=i;
+    			j++;
+    		}else {
+    			if(i!=0) i=lps[i-1];
+    			else {
+    				lps[j]=0;
+    				j++;
+    			}
+    		}
+    	}
+    	return lps;
+	}
+	
+	
+	boolean match(String pattern,String text,int[] lps) {
+		int i=0,j=0;
+    	int n=text.length(),m=pattern.length();
+    	while(i<n) {
+    		if(text.charAt(i)==pattern.charAt(j)) {
+    			i++;
+    			j++;
+    			if(j==m) {
+    				return true;
+    			}
+    		}
+    		else {
+    			if(j!=0) j=lps[j-1];
+    			else i++;
+    		}
+    
+    	}
+    	return false;
+	}
 }
