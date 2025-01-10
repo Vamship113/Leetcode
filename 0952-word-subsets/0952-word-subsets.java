317b0d8a1c8f3546d558b723a00f6b1a7771abc1
Vamship113 <92430906+Vamship113@users.noreply.github.com>
2025-01-10 14:16:18 +0530

    Time: 12 ms (92.44%), Space: 53.6 MB (94.19%) - LeetHub

---
+++ 0952-word-subsets/0952-word-subsets.java
@@ -0,0 +1,33 @@
+class Solution {
+    public List<String> wordSubsets(String[] words1, String[] words2) {
+    List<String> output= new ArrayList<>();
+        int[] max_freq = new int[26];
+        Arrays.fill(max_freq,0);
+        for(String word: words2){
+            int[] freq=count(word);
+            for(int i=0;i<26;i++)
+            max_freq[i]=Math.max(freq[i],max_freq[i]);
+        }
+        for(String word: words1){
+            int[] freq=count(word);
+            if(equal( max_freq,freq)) output.add(word);
+            
+        }
+        return output;
+        
+    }
+
+
+    int[] count(String s){
+        int[] t = new int[26];
+        for(int i=0;i<s.length();i++)
+        t[s.charAt(i)-'a']++;
+        return t;
+    }
+    boolean equal(int[] subset, int[] set){
+        for(int i=0;i<26;i++) {
+        	if(subset[i]!=0 && subset[i]>set[i]) return false;
+        }
+        return true;
+    }
+}
