109b34e5ca2cb7ae091d935d61dcd65e8a315dfe
Vamship113 <92430906+Vamship113@users.noreply.github.com>
2025-01-17 22:26:19 +0530

    Time: 2 ms (100%), Space: 62.5 MB (40.59%) - LeetHub

--- 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.java
+++ 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.java
@@ -1,14 +1,25 @@
-class Solution {
-    public boolean doesValidArrayExist(int[] derived) {
-        int sum=0;
-        for(int num:derived)
-        sum^=num;
-        return sum==0?true:false;
-    }
-}
+// class Solution {
+//     public boolean doesValidArrayExist(int[] derived) {
+//         int sum=0;
+//         for(int num:derived)
+//         sum^=num;
+//         return sum==0?true:false;
+//     }
+// }
 
 /*suppose
 1 1 0 and original array is 0 1 0
 derived is 0^1,1^0,0^0 since every element repeating twice overal sum=0
 since we diretly have the values 1  1 0 find 1 ^ 1 ^ 0 = should be zero 
 */
+
+public class Solution {
+
+    public boolean doesValidArrayExist(int[] derived) {
+        int sum = 0;
+        for (int num : derived) {
+            sum += num;
+        }
+        return sum % 2 == 0;
+    }
+}
