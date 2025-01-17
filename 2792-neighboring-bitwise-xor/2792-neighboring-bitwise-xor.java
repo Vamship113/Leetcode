adfee53ba6ca2fd79e5f97fa534404fb4167f21c
Vamship113 <92430906+Vamship113@users.noreply.github.com>
2025-01-17 22:24:06 +0530

    Time: 2 ms (100%), Space: 62.6 MB (32.94%) - LeetHub

--- 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.java
+++ 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.java
@@ -6,3 +6,9 @@ class Solution {
         return sum==0?true:false;
     }
 }
+
+/*suppose
+1 1 0 and original array is 0 1 0
+derived is 0^1,1^0,0^0 since every element repeating twice overal sum=0
+since we diretly have the values 1  1 0 find 1 ^ 1 ^ 0 = should be zero 
+*/
