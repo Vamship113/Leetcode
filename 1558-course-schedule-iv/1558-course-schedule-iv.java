510a0700489b08245d98865e7b369e6d9f2abd8e
Vamship113 <92430906+Vamship113@users.noreply.github.com>
2025-01-28 22:20:27 +0530

    Time: 38 ms (62.9%), Space: 48.2 MB (52.97%) - LeetHub

--- 1558-course-schedule-iv/1558-course-schedule-iv.java
+++ 1558-course-schedule-iv/1558-course-schedule-iv.java
@@ -1,52 +1,33 @@
-class Solution {
-
-    // Performs DFS and returns true if there's a path between src and target
-    // and false otherwise.
-    private boolean isPrerequisite(
-        Map<Integer, List<Integer>> adjList,
-        boolean[] visited,
-        int src,
-        int target
-    ) {
-        visited[src] = true;
-
-        if (src == target) {
-            return true;
-        }
-
-        boolean answer = false;
-        List<Integer> neighbors = adjList.getOrDefault(src, new ArrayList<>());
-        for (int adj : neighbors) {
-            if (!visited[adj]) {
-                answer =
-                    answer || isPrerequisite(adjList, visited, adj, target);
-            }
-        }
-        return answer;
-    }
+public class Solution {
 
     public List<Boolean> checkIfPrerequisite(
         int numCourses,
         int[][] prerequisites,
         int[][] queries
     ) {
-        Map<Integer, List<Integer>> adjList = new HashMap<>();
+        boolean[][] isPrerequisite = new boolean[numCourses][numCourses];
 
         for (int[] edge : prerequisites) {
-            adjList
-                .computeIfAbsent(edge[0], k -> new ArrayList<>())
-                .add(edge[1]);
+            isPrerequisite[edge[0]][edge[1]] = true;
         }
 
-        List<Boolean> result = new ArrayList<>();
-        for (int i = 0; i < queries.length; i++) {
-            // Reset the visited array for each query
-            boolean[] visited = new boolean[numCourses];
-            result.add(
-                isPrerequisite(adjList, visited, queries[i][0], queries[i][1])
-            );
+        for (int intermediate = 0; intermediate < numCourses; intermediate++) {
+            for (int src = 0; src < numCourses; src++) {
+                for (int target = 0; target < numCourses; target++) {
+                    // If there is a path i -> intermediate and intermediate -> j, then i -> j exists as well.
+                    isPrerequisite[src][target] =
+                        isPrerequisite[src][target] ||
+                        (isPrerequisite[src][intermediate] &&
+                            isPrerequisite[intermediate][target]);
+                }
+            }
         }
 
-        return result;
+        List<Boolean> answer = new ArrayList<>();
+        for (int[] query : queries) {
+            answer.add(isPrerequisite[query[0]][query[1]]);
+        }
+
+        return answer;
     }
 }
