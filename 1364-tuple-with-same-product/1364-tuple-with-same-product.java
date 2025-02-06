b6dd0a10cf7e2df6cc6affb4d11941997fd2a97e
Vamship113 <92430906+Vamship113@users.noreply.github.com>
2025-02-06 22:34:53 +0530

    Time: 207 ms (66.03%), Space: 80.7 MB (9.62%) - LeetHub

--- 1364-tuple-with-same-product/1364-tuple-with-same-product.java
+++ 1364-tuple-with-same-product/1364-tuple-with-same-product.java
@@ -1,18 +1,19 @@
+import java.util.*;
+
 class Solution {
     public int tupleSameProduct(int[] nums) {
-        int n=nums.length;
-        int n_tuples=0;
-        Map<Integer,Integer> map = new HashMap<>();
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = i + 1; j < n; j++) {
-                int prod = nums[i] * nums[j];
-                map.put(prod, map.getOrDefault(prod, 0) + 1);
-            }
-        }
+        int n_tuples = 0;
+        Map<Integer, Integer> map = new HashMap<>();
 
-        for (int value : map.values()) {
-            if (value > 1) {
-                n_tuples += (value * (value - 1) / 2) * 8;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                int prod = nums[i] * nums[j];
+                
+                // Directly count the number of tuples
+                n_tuples += map.getOrDefault(prod, 0) * 8;
+                
+                // Update frequency in the map
+                map.put(prod, map.getOrDefault(prod, 0) + 1);
             }
         }
         return n_tuples;
