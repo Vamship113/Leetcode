21266d628db6e74755f815bfc4b57d4a2cfd024b
Vamship113 <92430906+Vamship113@users.noreply.github.com>
2025-02-07 23:18:23 +0530

    Time: 41 ms (60.7%), Space: 94.4 MB (70.31%) - LeetHub

--- 3434-find-the-number-of-distinct-colors-among-the-balls/3434-find-the-number-of-distinct-colors-among-the-balls.java
+++ 3434-find-the-number-of-distinct-colors-among-the-balls/3434-find-the-number-of-distinct-colors-among-the-balls.java
@@ -1,22 +1,21 @@
 class Solution {
     public int[] queryResults(int limit, int[][] queries) {
-        Map<Integer, Integer> color = new HashMap<>();
-        Map<Integer, Integer> pos = new HashMap<>();
-        int i=0;
-        int[] result= new int[queries.length];
-        for(int[] query: queries){
-            int x=query[0];
-            int y=query[1];
-            if(pos.containsKey(x)){
-                int a=pos.get(x);
-                color.put(a, color.get(a) - 1);
-                if(color.get(a)==0) color.remove(a);
-            }           
-            pos.put(x,y);
-            color.put(y, color.getOrDefault(y, 0) + 1);
-            result[i++]=color.size();
+        Map<Integer,Integer> ball = new HashMap<>(), color = new HashMap<>();
+        int n = queries.length, distinct = 0;
+        int[] ans = new int[n];
+        for (int i = 0; i < n; i++){
+            int pos = queries[i][0], c = queries[i][1];
+            if(ball.containsKey(pos)){
+                int cnt = color.get(ball.get(pos)) - 1;
+                if(cnt == 0){ color.remove(ball.get(pos)); distinct--; }
+                else color.put(ball.get(pos), cnt);
             }
-            return result;
+            ball.put(pos, c);
+            int cnt = color.getOrDefault(c, 0) + 1;
+            color.put(c, cnt);
+            if(cnt == 1) distinct++;
+            ans[i] = distinct;
         }
-    
+        return ans;
     }
+}
