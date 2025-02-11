155f715d8f37e759a8f238470036594049511109
Vamship113 <92430906+Vamship113@users.noreply.github.com>
2025-02-11 22:41:48 +0530

    Time: 6 ms (45.15%), Space: 41.6 MB (98.84%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.java
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.java
@@ -1,29 +1,51 @@
 class Solution {
     public String removeOccurrences(String s, String part) {
-        StringBuilder res= new StringBuilder();
-        Stack<Character> stack = new Stack<>();
-        int strlen=s.length();
-        int partlen=part.length();
-        for(int i=0;i<strlen;i++){
-            stack.push(s.charAt(i));
-            if(stack.size()>=partlen && checkMatch(stack,part,partlen)){
-                for(int j=0;j<partlen;j++){
-                    stack.pop();
-                }
+        int n=part.length();
+        int[] lps = new int[n];
+        Arrays.fill(lps,0);
+        computeLPS(lps,part,n);
+        StringBuilder sb = new StringBuilder(s);
+        removeSubStringOccurance(sb,part,lps);
+        return sb.toString();
+    }
+    void computeLPS(int[] lps, String pattern,int n){
+        int i=0,j=1;
+        while(j<n){
+            if(pattern.charAt(i)==pattern.charAt(j)){
+                lps[j]=i+1;
+                i++;
+                j++;
+            }
+            else if(i>0){
+                i=lps[i-1];
+            }
+            else{
+                j++;
             }
         }
-        while(!stack.isEmpty()){
-            res.append(stack.pop());
-        }
-        return (res.reverse()).toString();
     }
-    boolean checkMatch(Stack<Character> stack,String part,int partlen){
-        Stack<Character> temp = new Stack<>();
-        temp.addAll(stack);
-        for(int i=partlen-1;i>=0;i--){
-            if(temp.isEmpty() || temp.peek()!=part.charAt(i)) return false;
-            temp.pop();
+    void removeSubStringOccurance(StringBuilder s,String part,int[] lps){
+        int m=s.length();
+        int n=part.length();
+        int i=0;
+        int j=0;
+        while(i<m){
+            if(s.charAt(i)==part.charAt(j)){
+                i++;
+                j++;
+            }
+            if(j==n){
+                s.delete(i-n,i);
+                m=s.length();
+                i=Math.max(0,i-2*n);
+                j=0;
+            }
+            if(i<m && s.charAt(i)!=part.charAt(j)){
+                if(j==0) i++;
+                else
+                j=lps[j-1];
+            }
         }
-        return true;
     }
 }
+
