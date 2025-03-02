56b72fd5d284378dddec542494d52d1b207a8abb
Vamship113 <92430906+Vamship113@users.noreply.github.com>
2025-03-02 20:55:21 +0530

    Time: 0 ms (100%), Space: 45 MB (88.77%) - LeetHub

--- 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.java
+++ 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.java
@@ -1,42 +1,28 @@
 class Solution {
     public int[][] mergeArrays(int[][] nums1, int[][] nums2) {
-        int i = 0, j = 0;
- 
-        List<int[]> resultList = new ArrayList<>();
-        
-
-        while (i < nums1.length && j < nums2.length) {
-            int id1 = nums1[i][0], val1 = nums1[i][1];
-            int id2 = nums2[j][0], val2 = nums2[j][1];
-            
-            if (id1 < id2) {
-                resultList.add(new int[]{id1, val1});
-                i++;
-            } else if (id2 < id1) {
-                resultList.add(new int[]{id2, val2});
-                j++;
-            } else {
-                resultList.add(new int[]{id1, val1 + val2});
+        int n1=nums1.length,n2=nums2.length,i=0,j=0;
+        ArrayList<int []>ans=new ArrayList();
+        while(i<n1&& j<n2){
+            if(nums1[i][0]==nums2[j][0]){
+                ans.add(new int[]{nums1[i][0],nums1[i][1]+nums2[j][1]});
                 i++;
                 j++;
             }
+            else if(nums1[i][0]<nums2[j][0]){
+                ans.add(nums1[i]);
+                i++;
+            }
+            else{
+                ans.add(nums2[j]);
+                j++;
+            }
         }
-
-        while (i < nums1.length) {
-            resultList.add(nums1[i]);
-            i++;
+        while(i<n1){
+            ans.add(nums1[i++]);
         }
-        
-
-        while (j < nums2.length) {
-            resultList.add(nums2[j]);
-            j++;
+        while(j<n2){
+            ans.add(nums2[j++]);
         }
-        int[][] result = new int[resultList.size()][2];
-        for (int k = 0; k < resultList.size(); k++) {
-            result[k] = resultList.get(k);
-        }
-        
-        return result;
+        return ans.toArray(new int[ans.size()][]);
     }
 }
