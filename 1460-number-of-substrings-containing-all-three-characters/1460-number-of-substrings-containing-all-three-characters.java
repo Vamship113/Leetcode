32474127cdf01bd74dcc5070978e8ec6bd42802c
Vamship113 <92430906+Vamship113@users.noreply.github.com>
2025-03-12 00:06:41 +0530

    Time: 11 ms (91.15%), Space: 44.7 MB (64.19%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.java
@@ -1,18 +1,16 @@
 class Solution {
+
     public int numberOfSubstrings(String s) {
-        int[] freq = new int[3];
-        int count=0;
-        int left=0,right=0,n=s.length();
-        while(right<n){
-            char c = s.charAt(right);
-            freq[c-'a']++;
-            while(freq[0]>0 && freq[1]>0 && freq[2]>0){
-                count+=(n-right);
-                freq[s.charAt(left)-'a']--;
-                left++;
-            }
-            right++;
+        int len = s.length();
+        // Track last position of a, b, c
+        int[] lastPos = { -1, -1, -1 };
+        int total = 0;
+
+        for (int pos = 0; pos < len; pos++) {
+            lastPos[s.charAt(pos) - 'a'] = pos;
+            total += 1 + Math.min(lastPos[0], Math.min(lastPos[1], lastPos[2]));
         }
-        return count;
+
+        return total;
     }
 }
