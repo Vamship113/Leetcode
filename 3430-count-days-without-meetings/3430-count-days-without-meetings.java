89d0ca3fd6710dda1090df518d451484d56ba074
Vamship113 <92430906+Vamship113@users.noreply.github.com>
2025-03-25 00:07:02 +0530

    Time: 42 ms (35.02%), Space: 100.2 MB (74.41%) - LeetHub

--- 3430-count-days-without-meetings/3430-count-days-without-meetings.java
+++ 3430-count-days-without-meetings/3430-count-days-without-meetings.java
@@ -1,29 +1,22 @@
-import java.util.*;
-
 class Solution {
+
     public int countDays(int days, int[][] meetings) {
-        List<int[]> time = new ArrayList<>();
+        int freeDays = 0, latestEnd = 0;
+
+        Arrays.sort(meetings, Comparator.comparingInt(a -> a[0]));
+
         for (int[] meeting : meetings) {
-            time.add(new int[]{meeting[0], 1});
-            time.add(new int[]{meeting[1] + 1, 0});
-        }
-        
-        // Sort the time list
-        time.sort((a, b) -> Integer.compare(a[0], b[0]));
-        
-        // Apply Line-Sweep 1D
-        int overlap = 0;
-        int count = time.get(0)[0] - 1; // Count free time before start
-        for (int i = 0; i < time.size() - 1; ++i) {
-            if (time.get(i)[1] == 0) overlap--;
-            else overlap++;
-            
-            if (overlap == 0) {
-                count += time.get(i + 1)[0] - time.get(i)[0];
+            int start = meeting[0], end = meeting[1];
+            if (start > latestEnd + 1) {
+                freeDays += start - latestEnd - 1;
             }
+  
+            latestEnd = Math.max(latestEnd, end);
         }
+
         
-        count += days - time.get(time.size() - 1)[0] + 1; // Count free time after end
-        return count;
+        freeDays += days - latestEnd;
+
+        return freeDays;
     }
 }
