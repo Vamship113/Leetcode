00c700c5faba16a67971167299324ac0cec6e233
Vamship113 <92430906+Vamship113@users.noreply.github.com>
2025-04-05 00:43:58 +0530

    Time: 0 ms (100%), Space: 44.6 MB (18.89%) - LeetHub

--- 1218-lowest-common-ancestor-of-deepest-leaves/1218-lowest-common-ancestor-of-deepest-leaves.java
+++ 1218-lowest-common-ancestor-of-deepest-leaves/1218-lowest-common-ancestor-of-deepest-leaves.java
@@ -14,25 +14,24 @@
  * }
  */
 class Solution {
+    TreeNode res;
+    int maxDepth = -1;
 
     public TreeNode lcaDeepestLeaves(TreeNode root) {
-        return dfs(root).getKey();
+        // time o(n), space o(n)
+        dfs(root, 0);
+        return res;
     }
 
-    private Pair<TreeNode, Integer> dfs(TreeNode root) {
-        if (root == null) {
-            return new Pair<>(null, 0);
+    private int dfs(TreeNode node, int depth) {
+        if (node == null) {
+            maxDepth = Math.max(maxDepth, depth);
+            return depth;
         }
 
-        Pair<TreeNode, Integer> left = dfs(root.left);
-        Pair<TreeNode, Integer> right = dfs(root.right);
-
-        if (left.getValue() > right.getValue()) {
-            return new Pair<>(left.getKey(), left.getValue() + 1);
-        }
-        if (left.getValue() < right.getValue()) {
-            return new Pair<>(right.getKey(), right.getValue() + 1);
-        }
-        return new Pair<>(root, left.getValue() + 1);
+        int left = dfs(node.left, depth+1);
+        int right = dfs(node.right, depth+1);
+        if (left == right && left == maxDepth) res = node;
+        return Math.max(left, right);
     }
 }
