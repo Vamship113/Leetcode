7ab5c72f7b414fa21d0313db8d1f589b5182cfba
Vamship113 <92430906+Vamship113@users.noreply.github.com>
2025-06-25 18:59:55 +0530

    Time: 6 ms (99.31%), Space: 44.7 MB (99.12%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.java
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.java
@@ -1,25 +1,28 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        int maxPile=0;
-        for(int pile : piles )
-        maxPile=Math.max(maxPile,pile);
-
-        int left=1,right=maxPile;
-        while(left<right){
+        int max_pile=0;
+        for(int pile: piles)
+        max_pile=Math.max(max_pile,pile);
+        // initialise left and right
+        int left=1,right=max_pile;
+        // System.out.println(left+" "+right);
+        while(left<=right){
             int mid=left+(right-left)/2;
-            if(canEat(piles,mid,h)) right=mid;
+            if(checkValid(piles,mid,h)) right=mid-1;
             else left=mid+1;
+
+            // System.out.println(left+" "+right+" "+mid);
         }
         return left;
-        
+
     }
-    boolean canEat(int[] piles,int k,int h){
-        int noOfHours=0;
-        for(int pile: piles){
-            if(pile%k==0) noOfHours+=pile/k;
-            else noOfHours+=pile/k+1;
+    boolean checkValid(int[] piles, int mid, int hrs) {
+        long count = 0; // use long to avoid overflow
+        for (int pile : piles) {
+            count += (pile + mid - 1) / mid; // ceiling division
+            if (count > hrs) return false; // early stop if already over time
         }
-        return noOfHours<=h;
-    } 
-    
+        return count <= hrs;
+    }
+
 }
