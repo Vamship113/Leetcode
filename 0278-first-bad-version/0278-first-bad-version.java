61f6690a53df50761a474860ee6d08b449d675f1
Vamship113 <92430906+Vamship113@users.noreply.github.com>
2025-06-25 14:10:38 +0530

    Time: 30 ms (13.55%), Space: 40.3 MB (65.4%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -7,8 +7,7 @@ public class Solution extends VersionControl {
         while(left<=right){
             int mid=left+(right-left)/2;
             if(isBadVersion(mid)){
-                if(mid==0) return mid;
-                else if(mid>0 && !isBadVersion(mid-1)) return mid;
+                if((mid==0) || (mid>0 && !isBadVersion(mid-1)))  return mid;
                 else right=mid-1;
             }
             else left=mid+1;
