7afd7678ea6c9bd63874ad03e2d862c03c328cae
Vamship113 <92430906+Vamship113@users.noreply.github.com>
2025-09-15 21:19:36 +0530

    Time: 0 ms (100%), Space: 44.9 MB (92.71%) - LeetHub

---
+++ 4012-earliest-time-to-finish-one-task/4012-earliest-time-to-finish-one-task.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int earliestTime(int[][] tasks) {
+        int minTime=Integer.MAX_VALUE;
+        for(int i=0;i<tasks.length;i++){
+            minTime=Math.min(minTime, tasks[i][0]+tasks[i][1]);
+        }
+        return minTime;
+    }
+}
