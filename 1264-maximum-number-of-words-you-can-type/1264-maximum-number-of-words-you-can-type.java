5e0f3f208322e072ef774ff3c54e92f7e874c25e
Vamship113 <92430906+Vamship113@users.noreply.github.com>
2025-09-15 21:09:35 +0530

    Time: 2 ms (91.65%), Space: 42.2 MB (73.09%) - LeetHub

---
+++ 1264-maximum-number-of-words-you-can-type/1264-maximum-number-of-words-you-can-type.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int canBeTypedWords(String text, String brokenLetters) {
+        int[] arr= new int[27];
+        for(char c: brokenLetters.toCharArray()){
+            arr[c-'a']++;
+        }
+        int count=0;
+        boolean found=false;
+        for(char c: text.toCharArray()){
+            if (!found & c!=' ')
+           { 
+            if(arr[c-'a']!=0) found =true;
+           }
+           if(c==' '){
+            if(!found) count++;
+            found=false;
+           }
+        }
+        if(!found) return ++count;
+        return count;
+    }
+}
